90526825d3025f705239f5e6f39d4e24094bcc76
jake514 <56373523+jake514@users.noreply.github.com>
2019-12-17 21:10:23 -0800

    first commit

---
+++ application.py
@@ -0,0 +1,25 @@
+# ----------------project 2 requirements-------------------------------------------------------------------------------------------------------------
+# display name: user prompt to enter display name. Needs to be remembered
+# channel creation: user can create new channel, cannot conflict with existing channel
+# messages view: can see & store up to 100 most recent message (upon opening a channel)
+# sending messages: text messages to other through channel. Contains display name & timestamp, all users on channel can see. Do not require reload
+# remember channel: remembers which channel user was previously on (if window closes)
+# personal touch: additional feature of my choosing
+#----------------------------------------------------------------------------------------------------------------------------------------------------
+import os
+
+from flask import Flask, session, render_template, request
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+# socketio = SocketIO(app)
+
+#------------------------------------------------------------app.routes
+@app.route("/")
+def index():
+    session["username"] = request.form.get("username")
+    if session["username"] is None:
+        return render_template('index.html')
+    else:
+        return render_template('chat.html')
