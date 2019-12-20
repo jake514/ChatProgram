60d6c2b77987528863a1ca6cb551de7ccee87477
jake514 <56373523+jake514@users.noreply.github.com>
2019-12-19 20:23:43 -0800

    Set up channel creation

--- application.py
+++ application.py
@@ -7,17 +7,59 @@
 # personal touch: additional feature of my choosing
 #----------------------------------------------------------------------------------------------------------------------------------------------------
 import os
-
 from flask import Flask, session, render_template, request
 from flask_socketio import SocketIO, emit
+from flask_session import Session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
-# socketio = SocketIO(app)
+socketio = SocketIO(app)
+
+
+# Check for environment variable
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
+
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Set up database
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
 
 #------------------------------------------------------------app.routes
 @app.route("/", methods=["POST", "GET"])
 def index():
-    return render_template('index.html')
+    channels = db.execute("SELECT * FROM channels").fetchall()
+    newChannel = ""
+    if newChannel != "" and newChannel != None:
+        db.execute("INSERT INTO channels(name) VALUES (:name)", {"name":newChannel})
+        channels = db.execute("SELECT * FROM channels").fetchall()
+    return render_template('index.html', channels=channels)
+    
+    
 
-    #MDN local storage
+
+@socketio.on("channelName")
+def create(channelName):
+    name = channelName["channelName"]
+    print(name)
+    if name != "" and name != None:
+        print("entered if statement")
+        nameExists= db.execute("SELECT name from channels WHERE name=:name", {"name":name}).fetchone()
+        print(nameExists)
+        if nameExists == None:
+            print("new channel name")
+            db.execute("INSERT INTO channels (name) VALUES (:name)", {"name":name})
+            db.commit()
+            print("added to database")
+    emit("newChannel", {"channelName":channelName}, broadcast=True)
+
+
+if __name__ == '__main__':
+    socketio.run(app)
--- static/js/app.js
+++ static/js/app.js
@@ -1,14 +1,34 @@
+
 document.addEventListener('DOMContentLoaded', function(){
     if(localStorage.getItem('username') == null || localStorage.getItem('username') == ""){
         var txt;
         var username = prompt("Please enter a username:", "")
-        if(username == null || username == "") {
+        while(username==null || username == ""){
             username = prompt("Please enter a valid username:", "")
+            print(username)
         }
         localStorage.setItem('username', username)
-        txt = "Hello " + username + ". Welcome to my chat app!"
+        txt = "Hello " + username + ". Welcome to the chat app!"
     }else{
         txt = "Hello " + localStorage.getItem('username') + (". Welcome back to the chat app!")
     }
     document.getElementById("welcome").innerHTML = txt;
+
+    //****  Setting up sockets  ****
+    var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port)
+    socket.on('connect', () => {
+        document.querySelector('.newChannelForm').onsubmit = () => {
+            var channelName = document.getElementById("newChannel").value;
+            console.log("emtting channel", channelName)
+            socket.emit('channelName', {'channelName': channelName})
+        }
+    });
+
+    socket.on('newChannel', data => {
+        const li = document.createElement('li');
+        console.log(data)
+        console.log(data.channelName)
+        li.innerHTML= `${data.channelName}`;
+        document.querySelector('#channelNames').append(li);
+    })
 });
